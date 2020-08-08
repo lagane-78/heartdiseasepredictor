ddf1f52f777dae3e79204d55d481b6ea52920396
trevorthompson78 <trevorthompson78@gmail.com>
2020-08-08 16:13:01 -0400

    pages

--- app.py
+++ app.py
@@ -6,8 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/model1')
+def model1():
+    return render_template('model1.html')
 
+@app.route('/model2')
+def model2():
+    return render_template('model2.html')
 
+@app.route('/team')
+def team():
+    return render_template('team.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
